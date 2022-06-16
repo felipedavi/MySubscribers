23a88a18b2509e4082543f9452edd20c004f3aa7
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-06-16 18:14:20 -0300

    feat: criação da toolbar do aplicativo

--- app/src/main/java/io/github/felipedavi/mysubscribers/MainActivity.kt
+++ app/src/main/java/io/github/felipedavi/mysubscribers/MainActivity.kt
@@ -2,10 +2,23 @@ package io.github.felipedavi.mysubscribers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import io.github.felipedavi.mysubscribers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.appToolbar.setupWithNavController(navController, appBarConfiguration)
+
+        val view = binding.root
+        setContentView(view)
     }
 }
